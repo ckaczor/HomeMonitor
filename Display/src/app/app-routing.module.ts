50599a8dc4dcdcda1c4b73aef54388bd81021e3e
Chris Kaczor <chris@kaczorzoo.net>
2019-10-08 19:17:34 -0400

    Remove weather and laundry from sidenav and routing

--- Display/src/app/app-routing.module.ts
+++ Display/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LaundryComponent } from './components/laundry/laundry.component';
 import { WeatherChartsComponent } from './components/weather/charts/weather-charts.component';
-import { WeatherCurrentComponent } from './components/weather/current/weather-current.component';
 
 const routes: Routes = [
     {
@@ -11,16 +9,8 @@ const routes: Routes = [
         component: DashboardComponent
     },
     {
-        path: 'weather',
-        component: WeatherCurrentComponent
-    },
-    {
         path: 'weather-charts',
         component: WeatherChartsComponent
-    },
-    {
-        path: 'laundry',
-        component: LaundryComponent
     }
 ];
 
