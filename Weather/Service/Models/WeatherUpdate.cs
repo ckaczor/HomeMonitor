4c4d01418f770bcc2a87663c4a2793eca2e5267b
Chris Kaczor <chris@kaczorzoo.net>
2020-11-17 19:20:37 -0500

    Update service to use base model

--- Weather/Service/Models/WeatherUpdate.cs
+++ Weather/Service/Models/WeatherUpdate.cs
@@ -1,61 +1,16 @@
-using System;
-using System.Linq;
 using ChrisKaczor.HomeMonitor.Weather.Models;
 using ChrisKaczor.HomeMonitor.Weather.Service.Data;
 using DecimalMath;
 using JetBrains.Annotations;
 using MathNet.Numerics;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+using System.Linq;
 
 namespace ChrisKaczor.HomeMonitor.Weather.Service.Models
 {
     [PublicAPI]
-    public class WeatherUpdate
+    public class WeatherUpdate : WeatherUpdateBase
     {
         private readonly Database _database;
-
-        [JsonConverter(typeof(StringEnumConverter))]
-        public MessageType Type { get; set; }
-
-        public string Message { get; set; }
-
-        public DateTimeOffset Timestamp { get; set; }
-
-        [JsonConverter(typeof(StringEnumConverter))]
-        public WindDirection WindDirection { get; set; }
-
-        public decimal WindSpeed { get; set; }
-
-        public decimal Humidity { get; set; }
-
-        public decimal Rain { get; set; }
-
-        public decimal Pressure { get; set; }
-
-        public decimal Temperature { get; set; }
-
-        public decimal LightLevel { get; set; }
-
-        public decimal Latitude { get; set; }
-
-        public decimal Longitude { get; set; }
-
-        public decimal Altitude { get; set; }
-
-        public int SatelliteCount { get; set; }
-
-        public DateTimeOffset GpsTimestamp { get; set; }
-
-        public decimal? WindChill { get; set; }
-
-        public decimal? HeatIndex { get; set; }
-
-        public decimal DewPoint { get; set; }
-
-        public decimal PressureDifferenceThreeHour { get; set; }
-
-        public decimal RainLastHour { get; set; }
 
         public WeatherUpdate(WeatherMessage weatherMessage, Database database)
         {
@@ -89,31 +44,6 @@ namespace ChrisKaczor.HomeMonitor.Weather.Service.Models
             RainLastHour = _database.GetReadingValueSum(WeatherValueType.Rain, Timestamp.AddHours(-1), Timestamp).Result;
         }
 
-        private void CalculatePressureTrend()
-        {
-            var pressureData = _database.GetReadingValueHistory(WeatherValueType.Pressure, Timestamp.AddHours(-3), Timestamp).Result;
-
-            var xData = pressureData.Select(p => (double)p.Timestamp.ToUnixTimeSeconds()).ToArray();
-            var yData = pressureData.Select(p => (double)p.Value / 100.0).ToArray();
-
-            var lineFunction = Fit.LineFunc(xData, yData);
-
-            PressureDifferenceThreeHour = (decimal)(lineFunction(xData.Last()) - lineFunction(xData[0]));
-        }
-
-        private void CalculateHeatIndex()
-        {
-            var temperature = Temperature;
-            var humidity = Humidity;
-
-            if (temperature.IsBetween(80, 100) && humidity.IsBetween(40, 100))
-            {
-                HeatIndex = -42.379m + 2.04901523m * temperature + 10.14333127m * humidity - .22475541m * temperature * humidity - .00683783m * temperature * temperature -
-                            .05481717m * humidity * humidity + .00122874m * temperature * temperature * humidity + .00085282m * temperature * humidity * humidity -
-                            .00000199m * temperature * temperature * humidity * humidity;
-            }
-        }
-
         private void CalculateWindChill()
         {
             var temperatureInF = Temperature;
@@ -138,6 +68,34 @@ namespace ChrisKaczor.HomeMonitor.Weather.Service.Models
             var dewPointInC = numerator / denominator;
 
             DewPoint = dewPointInC * 9.0m / 5.0m + 32.0m;
+        }
+
+        private void CalculatePressureTrend()
+        {
+            var pressureData = _database
+                .GetReadingValueHistory(WeatherValueType.Pressure, Timestamp.AddHours(-3), Timestamp).Result.ToList();
+
+            var xData = pressureData.Select(p => (double)p.Timestamp.ToUnixTimeSeconds()).ToArray();
+            var yData = pressureData.Select(p => (double)p.Value / 100.0).ToArray();
+
+            var lineFunction = Fit.LineFunc(xData, yData);
+
+            PressureDifferenceThreeHour = (decimal)(lineFunction(xData.Last()) - lineFunction(xData[0]));
+        }
+
+        private void CalculateHeatIndex()
+        {
+            var temperature = Temperature;
+            var humidity = Humidity;
+
+            if (temperature.IsBetween(80, 100) && humidity.IsBetween(40, 100))
+            {
+                HeatIndex = -42.379m + 2.04901523m * temperature + 10.14333127m * humidity -
+                            .22475541m * temperature * humidity - .00683783m * temperature * temperature -
+                            .05481717m * humidity * humidity + .00122874m * temperature * temperature * humidity +
+                            .00085282m * temperature * humidity * humidity -
+                            .00000199m * temperature * temperature * humidity * humidity;
+            }
         }
     }
 }
