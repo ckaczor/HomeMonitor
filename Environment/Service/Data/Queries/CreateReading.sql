6460430a69f4420fa0d28d8ec8249b6b93fb8482
Chris Kaczor <chris@kaczorzoo.net>
2024-02-01 16:39:45 -0500

    Tweak database setup

--- Environment/Service/Data/Queries/CreateReading.sql
+++ Environment/Service/Data/Queries/CreateReading.sql
@@ -1,30 +1,24 @@
-INSERT INTO 
-    reading 
-    (
-        time, 
-        name, 
-        model, 
-        temperature, 
-        pressure, 
-        humidity, 
-        luminance, 
-        gas_resistance, 
-        color_temperature, 
-        air_quality_index
-    )
-VALUES 
-    (
-        @Timestamp, 
-        @Name, 
-        @Model, 
-        @Temperature, 
-        @Pressure, 
-        @Humidity, 
-        @Luminance, 
-        @GasResistance, 
-        @ColorTemperature, 
-        @AirQualityIndex
-    ) 
-ON CONFLICT 
-    ON CONSTRAINT reading_pk 
-        DO NOTHING
+INSERT INTO reading(
+    time,
+    name,
+    model,
+    temperature,
+    pressure,
+    humidity,
+    luminance,
+    gas_resistance,
+    color_temperature,
+    air_quality_index)
+VALUES (
+    @Timestamp,
+    @Name,
+    @Model,
+    @Temperature,
+    @Pressure,
+    @Humidity,
+    @Luminance,
+    @GasResistance,
+    @ColorTemperature,
+    @AirQualityIndex)
+ON CONFLICT
+    DO NOTHING
--- Environment/Service/Data/Schema/1-Initial Schema.sql
+++ Environment/Service/Data/Schema/1-Initial Schema.sql
@@ -1,17 +1,26 @@
-CREATE TABLE
-    reading (
-        time timestamptz NOT NULL,
-        name text NOT NULL,
-        model text NOT NULL,
-        temperature DECIMAL NOT NULL,
-        pressure DECIMAL NOT NULL,
-        humidity DECIMAL NOT NULL,
-        luminance INT NOT NULL,
-        gas_resistance INT NOT NULL,
-        color_temperature INT NOT NULL,
-        air_quality_index DECIMAL NOT NULL,
-        CONSTRAINT reading_pk PRIMARY KEY (time, name, model)
-    );
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+CREATE TABLE reading(
+  time timestamptz NOT NULL,
+  name text NOT NULL,
+  model text NOT NULL,
+  temperature DECIMAL NOT NULL,
+  pressure DECIMAL NOT NULL,
+  humidity DECIMAL NOT NULL,
+  luminance int NOT NULL,
+  gas_resistance int NOT NULL,
+  color_temperature int NOT NULL,
+  air_quality_index DECIMAL NOT NULL,
+  CONSTRAINT reading_pk PRIMARY KEY (time, name, model)
+);
 
 SELECT
-    create_hypertable('reading', by_range('time'));
+  create_hypertable('reading', by_range('time'));
+
+ALTER TABLE reading SET (
+  timescaledb.compress, 
+  timescaledb.compress_segmentby = 'name, model', 
+  timescaledb.compress_orderby = 'time DESC');
+
+SELECT
+  add_compression_policy('reading', INTERVAL '7 days');
