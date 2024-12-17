4dbedee94b791237386c40b161126e3589d37efe
Chris Kaczor <chris@kaczorzoo.net>
2024-12-17 19:49:23 +0000

    Fix token

--- WebDisplay/nginx/entrypoint.sh
+++ WebDisplay/nginx/entrypoint.sh
@@ -5,3 +5,4 @@ sed -i -e "s~#HOME_ASSISTANT_URL#~$HOME_ASSISTANT_URL~g" /usr/share/nginx/html/a
 sed -i -e "s~#HOME_ASSISTANT_TOKEN#~$HOME_ASSISTANT_TOKEN~g" /usr/share/nginx/html/assets/*
 sed -i -e "s~#GARAGE_DEVICE#~$GARAGE_DEVICE~g" /usr/share/nginx/html/assets/*
 sed -i -e "s~#ALARM_DEVICE#~$ALARM_DEVICE~g" /usr/share/nginx/html/assets/*
+sed -i -e "s~#CALENDAR_EMBED_URL#~$CALENDAR_EMBED_URL~g" /usr/share/nginx/html/assets/*
