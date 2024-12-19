200a07d6ba5b17e741577661bb2932e3e49c2ec6
Chris Kaczor <chris@kaczorzoo.net>
2024-12-19 09:21:03 -0500

    Add optional holidays to upcoming

--- Calendar/Service/Controllers/CalendarController.cs
+++ Calendar/Service/Controllers/CalendarController.cs
@@ -7,10 +7,9 @@ namespace ChrisKaczor.HomeMonitor.Calendar.Service.Controllers;
 [ApiController]
 public class CalendarController(IConfiguration configuration, HttpClient httpClient) : ControllerBase
 {
-    [HttpGet("upcoming")]
-    public async Task<ActionResult<IEnumerable<CalendarEntry>>> GetUpcoming([FromQuery] int days = 1)
+    private async Task<IEnumerable<CalendarEntry>> GetCalendarEntries(string calendarUrl, int days)
     {
-        var data = await httpClient.GetStringAsync(configuration["Calendar:PersonalUrl"]);
+        var data = await httpClient.GetStringAsync(calendarUrl);
 
         var calendar = Ical.Net.Calendar.Load(data);
 
@@ -21,6 +20,20 @@ public class CalendarController(IConfiguration configuration, HttpClient httpCli
             .GetOccurrences(start, end)
             .Select(o => new CalendarEntry(o))
             .OrderBy(ce => ce.Start);
+
+        return calendarEntries;
+    }
+
+    [HttpGet("upcoming")]
+    public async Task<ActionResult<IEnumerable<CalendarEntry>>> GetUpcoming([FromQuery] int days = 1, [FromQuery] bool includeHolidays = false)
+    {
+        var calendarEntries = await GetCalendarEntries(configuration["Calendar:PersonalUrl"]!, days);
+
+        if (!includeHolidays) 
+            return Ok(calendarEntries);
+
+        var holidayEntries = await GetCalendarEntries(configuration["Calendar:HolidayUrl"]!, days);
+        calendarEntries = calendarEntries.Concat(holidayEntries).OrderBy(c => c.Start);
 
         return Ok(calendarEntries);
     }
